95b2569ba135050182a1577ca7cfa242be546374
kddkdkk <dlwjddnrbb71@naver.com>
2021-11-13 00:35:36 +0900

    비밀번호 변경 구현

--- src/main/java/com/MateStudy/MateStudy/controller/AccountController.java
+++ src/main/java/com/MateStudy/MateStudy/controller/AccountController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,6 +20,12 @@ public class AccountController {
 
     @Autowired
     private AccountService accountService;
+
+    @PostMapping("/modify")
+    public String modify(UserRequest request) throws Exception {
+        accountService.modify(request.getId(), request.getPassword());
+        return "redirect:/";
+    }
 
     @PostMapping("/login")
     public String login(UserRequest request) throws Exception {
