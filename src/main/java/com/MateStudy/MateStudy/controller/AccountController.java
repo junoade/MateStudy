89b42936f8df46119fd5c68445632ee7bac88f4d
ChoiJunho <ajchoi0928@gmail.com>
2021-11-12 20:56:01 +0900

    Feat : DB 회원정보 조회후 페이지 이동
    스프링 시큐리티 와 세션처리를 위해 진행 중이나 잘 되지 않아 차후 스프린트에서 작업예정

--- src/main/java/com/MateStudy/MateStudy/controller/AccountController.java
+++ src/main/java/com/MateStudy/MateStudy/controller/AccountController.java
@@ -6,16 +6,13 @@ import com.MateStudy.MateStudy.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
 public class AccountController {
@@ -24,13 +21,15 @@ public class AccountController {
     private AccountService accountService;
 
     @PostMapping("/login")
-    public ResponseEntity login( UserRequest request) throws Exception {
+    public String login(UserRequest request) throws Exception {
         log.info("로그인 시도");
-        if(accountService.login(request.getId(), request.getPassword()).equals("Success")){
+        if (accountService.login(request.getId(), request.getPassword()).equals("Success")) {
             log.info("success");
-            return new ResponseEntity(HttpStatus.OK);
+            //return new ResponseEntity(HttpStatus.OK);
+            return "redirect:/main";
         }
         log.info("failed");
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        //return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        return "redirect:/";
     }
 }
