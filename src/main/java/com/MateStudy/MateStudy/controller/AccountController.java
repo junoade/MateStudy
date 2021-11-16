63e571775a44ae641cf6fa318ac7fd65042bacf8
ChoiJunho <ajchoi0928@gmail.com>
2021-11-17 00:59:13 +0900

    Feat: 로그인 이동 후 사용자 이름 보여주기
    추후 세션 이용하게 되면 세션 정보 이용해서 TODO 해결하기

--- src/main/java/com/MateStudy/MateStudy/controller/AccountController.java
+++ src/main/java/com/MateStudy/MateStudy/controller/AccountController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Slf4j
@@ -28,17 +31,30 @@ public class AccountController {
     }
 
     @PostMapping("/login")
-    public String login(UserRequest request) throws Exception {
+    public String login(UserRequest request, RedirectAttributes rttr) throws Exception {
         log.info("로그인 시도");
+        ModelAndView mav = new ModelAndView();
+        /* TODO 로그인 세션 정보 연동 하기 */
         if (accountService.login(request.getId(), request.getPassword()).equals("Success")) {
             log.info("success");
-            //return new ResponseEntity(HttpStatus.OK);
-            return "redirect:/main";
+
+            /* 로그인 회원 정보 main 페이지로 반환  */
+            String username = accountService.getName(request.getId());
+            /* TODO 차후 로그인 세션 정보 만들기 */
+            rttr.addAttribute("userName", username);
+
+            return "redirect:/afterLogin";
         }
         log.info("failed");
         //return new ResponseEntity(HttpStatus.BAD_REQUEST);
         return "redirect:/";
 
         //
+    }
+
+    // TODO 로그아웃시 세션 만료 명시하기
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/";
     }
 }
--- src/main/java/com/MateStudy/MateStudy/service/AccountService.java
+++ src/main/java/com/MateStudy/MateStudy/service/AccountService.java
@@ -2,5 +2,9 @@ package com.MateStudy.MateStudy.service;
 
 public interface AccountService {
     String login(Long id, String pwd) throws Exception;
-    void modify(Long id, String pwd) ;
+
+    void modify(Long id, String pwd);
+
+    String getName(Long id) throws Exception;
+
 }
