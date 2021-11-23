24f0f87f61e54b29e63689787d2f825d93dac636
BBongKim <kbg0099@naver.com>
2021-11-23 22:10:10 +0900

    학생 과제화면 추가

--- src/main/java/com/MateStudy/MateStudy/controller/MainController.java
+++ src/main/java/com/MateStudy/MateStudy/controller/MainController.java
@@ -65,4 +65,13 @@ public class MainController {
         return "denied";
     }
 
+    @GetMapping("/hw-admin")
+    public String homeWorkAdmin(){
+        return "homework-admin";
+    }
+
+    @GetMapping("/hw-student")
+    public String homeWorkStudent(){
+        return "homework-student";
+    }
 }
