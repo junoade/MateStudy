295bfc5b105e2315df5b66db93a81d1947f1e838
ChoiJunho <ajchoi0928@gmail.com>
2021-11-24 21:20:55 +0900

    Feat : 배정된 교수자 테이블, 배정된 학생 테이블 과 각각 과제 공지 테이블, 과제 제출 테이블과의 관계 및 파일 과의 관계 설정

---
+++ src/main/java/com/MateStudy/MateStudy/domain/homework/Submit_Homework.java
@@ -0,0 +1,70 @@
+package com.MateStudy.MateStudy.domain.homework;
+
+import com.MateStudy.MateStudy.domain.account.Member;
+import com.MateStudy.MateStudy.domain.common.BaseEntity;
+import com.MateStudy.MateStudy.domain.lecture.Taking_Lecture;
+import com.MateStudy.MateStudy.domain.lecture.TeachingLecId;
+import com.MateStudy.MateStudy.domain.lecture.Teaching_Lecture;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ *  등록된 실습이 갖는 여러 과제들의  관계
+ *  TEACH_LECTURE : HOMEWORK
+ *  Maximum cardinality 1:N
+ *  Minimum cardinality M:O
+ */
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = {"hwId","stId","instId", "lecCode", "subCode"}) // 성능을 위해 fetch Lazy 타입으로 종종한다. 이때 사용
+@Table(name = "SUBMIT_HW")
+public class Submit_Homework extends BaseEntity {
+    @Id
+    private Long submitId; // Wrapper 클래스인 Long 쓰는 이유, long과 같은 primitive 타입의 기본값은 0인데, 데이터베이스에서 데이터가 없다는 의미인지, id가 0인지 혼동
+
+    /* TAKE_LECUTE 엔티티의 stId - 등록된 실습 강좌에 속하는 학생이 등록하는 과제 */
+    /*@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stId", nullable = true)
+    private Taking_Lecture stId;*/
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hwId", nullable = false)
+    private Assign_Homework hwId;
+    /* TEACH_LECTURE 엔티티와 맵핑 (등록되어있는 실습 강좌에서 등록한 과제 */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "instId", referencedColumnName = "instId",insertable = false, updatable = false),
+            @JoinColumn(name = "lecCode", referencedColumnName = "lecCode",insertable = false, updatable = false),
+            @JoinColumn(name = "subCode", referencedColumnName = "subCode",insertable = false, updatable = false),
+            @JoinColumn(name = "stId", nullable = true, referencedColumnName = "stId", insertable = false, updatable = false)
+    })
+    private Taking_Lecture t_lec;
+
+    /* LECTURE 엔티티와 맵핑 (등록되어있는 실습 강좌의 분반정보 가르킴)*/
+    @Column(name="stId", nullable = false)
+    private String stId;
+
+    @Column(name="instId", nullable = false)
+    private String instId;
+    @Column(name="lecCode", nullable = false)
+    private String lecCode;
+    @Column(name="subCode", nullable = false)
+    private Long subCode;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="content")
+    private String content;
+
+    @Column(name="grade")
+    private Integer grade;
+
+}
---
+++ src/main/java/com/MateStudy/MateStudy/domain/homework/Submit_Hw_File.java
@@ -0,0 +1,33 @@
+package com.MateStudy.MateStudy.domain.homework;
+
+import com.MateStudy.MateStudy.domain.common.BaseEntity;
+import com.MateStudy.MateStudy.domain.common.File;
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * 부여된 실습과제 엔티티와 File 엔티티간의 관계
+ */
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = {"submitId", "Fid"}) // 성능을 위해 fetch Lazy 타입으로 종종한다. 이때 사용
+@Table(name = "SUBMIT_HW_FILE")
+@IdClass(Shw_FileId.class)
+public class Submit_Hw_File extends BaseEntity {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="submitId")
+    private Submit_Homework submitId;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="Fid")
+    private File Fid; //우리 패키지의 File 클래스임 조심
+
+}
