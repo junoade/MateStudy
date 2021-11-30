2af13bfd567a5c49a3fd635af35b35d92570f6cf
ChoiJunho <ajchoi0928@gmail.com>
2021-12-01 05:15:13 +0900

    Fix: 출석 관련해서 추가해보려다가 잘 안되어서 버튼은 막아놓음

--- src/main/java/com/MateStudy/MateStudy/repository/attend/AttendRepository.java
+++ src/main/java/com/MateStudy/MateStudy/repository/attend/AttendRepository.java
@@ -2,6 +2,22 @@ package com.MateStudy.MateStudy.repository.attend;
 
 import com.MateStudy.MateStudy.domain.attendance.Attend;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AttendRepository extends JpaRepository<Attend, Long> {
+
+    @Query(value="SELECT A FROM Attend A WHERE A.lecCode = :lecCode AND A.subCode = :subCode")
+    List<Attend> getEveryAttend(@Param("lecCode") String lecCode, @Param("subCode") Long subCode);
+
+
+    @Query(value="SELECT DISTINCT A FROM Attend A WHERE A.stId =:stId AND A.instId = :instId AND A.lecCode =:lecCode AND A.subCode = :subCode")
+    List<Attend> getMyAttendRecord(@Param("stId") String stId, @Param("instId") String instId, @Param("lecCode") String lecCode, @Param("subCode") Long subCode);
+
+    @Query(value="SELECT DISTINCT A FROM Attend A WHERE A.stId =:stId AND A.instId = :instId AND A.lecCode =:lecCode AND A.subCode = :subCode AND A.week = :week")
+    Optional<Attend> getWeekAttend(@Param("stId") String stId, @Param("instId") String instId, @Param("lecCode") String lecCode, @Param("subCode") Long subCode);
+
 }
